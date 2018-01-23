258ba7ad63e893dd702019be919e2b075db4b585
Zahir Yahya <theeYahya@gmail.com>
2018-01-23 14:02:59 -0800

    Initial Commit

---
+++ Fashion App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fashion App
+//
+//  Created by Zahir Yahya on 1/23/18.
+//  Copyright © 2018 Ignornat Monky Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+use_frameworks!
+
+target 'Fashion App' do
+  # Pods for Fashion App
+  pod 'Alamofire'
+  pod 'SwiftyJSON'
+  pod 'CryptoSwift'
+
+end
